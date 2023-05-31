21774c1c348fbefda0a47e5becd47c9b5a11a6ca
Softlabltd.com <freelancernishad123@gmail.com>
2023-05-31 16:46:48 +0600

    up

--- resources/views/frontlayout.blade.php
+++ resources/views/frontlayout.blade.php
@@ -153,7 +153,7 @@ p.help.text-right {
 
 
 
-    <script src="{{ asset('js/frontend.js?ver=1.0.15') }}" async ></script>
+    <script src="{{ asset('js/frontend.js?ver=1.0.16') }}" async ></script>
 
 <script>
 
